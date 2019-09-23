38fb213f02bfe5bebda5264adaa54cb80c9af4e7
HamedZVand <sunrise.freedom@gmail.com>
2019-09-23 11:16:27 +0330

    add Alamut.AspNet.Dependency project

---
+++ src/Alamut.AspNet.DependencyInjection/UserInfoServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Alamut.Abstractions.Principal;
+using Alamut.AspNet.Principal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alamut.Extensions.Microsoft.DependencyInjection
+{
+    /// <summary>
+    /// Extensions to Register IUserInfo
+    /// </summary>
+    public static class UserInfoServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers <see cref="IUserInfo" /> by <see cref="UserInfoByHttpContext" /> implementation
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="byHttpContext"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddUserInfo(this IServiceCollection services, bool byHttpContext = true)
+            => services.AddScoped<IUserInfo, UserInfoByHttpContext>();
+    }
+}
