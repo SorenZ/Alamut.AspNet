af2cabbd477264883a0cfd384f1cc6947a7182c8
Mohammad Dayyan <Mohammad.Dayyan@outlook.com>
2019-11-10 11:40:38 +0330

    Handle Exception in UserInfo

--- src/Alamut.AspNet/Principal/UserInfoByHttpContext.cs
+++ src/Alamut.AspNet/Principal/UserInfoByHttpContext.cs
@@ -13,21 +13,21 @@ namespace Alamut.AspNet.Principal
             _httpContextAccessor = httpContextAccessor;
         }
 
-        public string UserName => _httpContextAccessor.HttpContext.User?.Identity?.Name;
+        public string UserName => _httpContextAccessor.HttpContext?.User?.Identity?.Name;
         
-        public string Name => _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Name)?.Value;
+        public string Name => _httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.Name)?.Value;
         
-        public string GivenName => _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.GivenName)?.Value;
+        public string GivenName => _httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.GivenName)?.Value;
         
-        public string UserId => _httpContextAccessor.HttpContext.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        public string UserId => _httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         
-        public string UserAgent => _httpContextAccessor.HttpContext.Request.Headers[HeaderNames.UserAgent].ToString();
+        public string UserAgent => _httpContextAccessor.HttpContext?.Request?.Headers[HeaderNames.UserAgent].ToString();
 
-        public string RequestPath => _httpContextAccessor.HttpContext.Request.Path;
+        public string RequestPath => _httpContextAccessor.HttpContext?.Request?.Path;
 
-        public string RequestQueryString => _httpContextAccessor.HttpContext.Request.QueryString.ToString();
+        public string RequestQueryString => _httpContextAccessor.HttpContext?.Request?.QueryString.ToString();
 
-        public string RequestMethod => _httpContextAccessor.HttpContext.Request.Method;
+        public string RequestMethod => _httpContextAccessor.HttpContext?.Request?.Method;
 
         public string GetClientIpAddress(bool tryUseXForwardHeader = true)
         {
